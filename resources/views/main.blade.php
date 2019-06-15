e4981cdf7a168135e27ef9585e8df10bd332a3c1
Chondrima <chondrima.xponent@gmail.com>
2019-06-15 13:51:10 +0600

    second commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @include('partials._head')    
+</head>
+
+<body>
+
+@include('partials._nav')
+
+<br>
+
+<div class="container">
+    @yield('content')
+
+    @include('partials._footer')
+</div>
+
+@include('partials._javascript')
+
+@yield('script')
+
+</body>
+
+</html>
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<hr>
+<hr>
+<p class="text-center">Copyright Chondrima - All Rights Reserved</p>
